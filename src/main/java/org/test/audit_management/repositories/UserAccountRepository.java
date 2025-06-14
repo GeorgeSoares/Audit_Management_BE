ba0065404c9b30ce314759360d58a521db2bdc31
George Soares <georgea984@gmail.com>
2025-06-14 22:33:39 +0200

    refactor: deletion of files to the adjusment on the project structure

--- src/main/java/org/test/audit_management/repositories/UserAccountRepository.java
+++
@@ -1,9 +0,0 @@
-package org.test.audit_management.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import org.test.audit_management.models.UserAccount;
-
-@Repository
-public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
-}

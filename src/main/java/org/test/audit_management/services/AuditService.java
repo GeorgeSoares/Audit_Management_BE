5b77380be750347df7347dde25e0ae185b15c7ed
George Soares <georgea984@gmail.com>
2025-06-22 23:03:10 +0200

    docs: added comments to the Audit Service

--- src/main/java/org/test/audit_management/services/AuditService.java
+++ src/main/java/org/test/audit_management/services/AuditService.java
@@ -13,12 +13,26 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+/**
+ * This Service here is responsible for the implementation of the business logic related to the Audits, it covers the
+ * creation, update, retrieval e deletion of audits.
+ * For the purpose of the interaction with the db, the bean of the repository is injected.
+ */
+
+
 @Service
 public class AuditService {
 
     @Autowired
     private AuditRepository auditRepository;
 
+    /**
+     * This method creates an audit based on the corresponding DTO. It sets the attributes according to the data given as input
+     * on the Audit form and persists the audit on the database.
+     *
+     * @param auditRequestDTO DTO with the information of the audit.
+     * @return true if the audit was successfully persisted in the database.
+     */
     public boolean createNewAudit(AuditRequestDTO auditRequestDTO) {
         Audit newAudit = new Audit();
 
@@ -38,16 +52,34 @@ public class AuditService {
         }
     }
 
+    /**
+     * This method returns a list of all audits in the database.
+     * @return list of audits present in the database.
+     */
     public List<Audit> listAudits() {
         return auditRepository.findAll();
     }
 
+
+    /**
+     * This method returns a specific audit.
+     * @param auditId GUID of the audited to be consulted.
+     * @return audit if the GUID is valid.
+     */
     public Optional<Audit> getAudit(UUID auditId) {
         Optional<Audit> audit = auditRepository.findById(auditId);
 
         return audit;
     }
 
+    /**
+     * This method updates an existing audit with the data provided by the corresponding DTO.
+     *
+     * @param auditInput Object of the audit to be updated.
+     * @param auditEditDTO DTO containing the updated information of the audit.
+     *
+     * @return true if the updated audit object was persisted successfully.
+     */
     public boolean updateExistingAudit(Audit auditInput, AuditEditDTO auditEditDTO) {
         Optional<Audit> auditOptional = auditRepository.findById(auditInput.getAuditId());
 
@@ -72,6 +104,13 @@ public class AuditService {
         }
     }
 
+
+    /**
+     * Deletes an audit from the database.
+     *
+     * @param auditId GUID of the audit to be deleted.
+     * @return true is the audit was successfully deleted.
+     */
     public boolean deleteAudit(UUID auditId) {
         Optional<Audit> auditOptional = auditRepository.findById(auditId);
 

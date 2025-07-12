936d8ff3234a8894665790749ae26eb50cb28aa5
George Soares <georgea984@gmail.com>
2025-07-12 23:40:25 +0200

    docs: added comments to the Audit and Finding repositories

--- src/main/java/org/test/audit_management/repositories/AuditRepository.java
+++ src/main/java/org/test/audit_management/repositories/AuditRepository.java
@@ -6,6 +6,14 @@ import org.test.audit_management.models.Audit;
 
 import java.util.UUID;
 
+/**
+ * Repository interface for performing CRUD operations on Audit entities.
+ *
+ * This interface extends JpaRepository, which provides methods for standard database
+ * interactions such as saving, finding, deleting, and updating entities.
+ *
+ * The entity managed here is the Audit, and its primary key type is UUID.
+ */
 @Repository
 public interface AuditRepository extends JpaRepository<Audit, UUID> {
 }
